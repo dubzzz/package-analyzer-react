9def9f73ed61d33d740dab4f8d56565f65730f9a
Nicolas DUBIEN <github@dubien.org>
2018-11-01 17:46:22 +0100

    Basic redux-saga setup

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,4 +1,10 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-export default createStore(rootReducer);
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+export default createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
