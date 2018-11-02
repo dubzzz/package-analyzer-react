04b2892b2c95437202e0e449a7968f676b13a96a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-02 02:15:12 +0000

    Deploy dubzzz/package-analyzer-react to github.com/dubzzz/package-analyzer-react.git:gh-pages

---
+++ precache-manifest.b741a50ffd4a28a53200778bf741b290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3cb8832df1f11d027f9",
+    "url": "/static/js/main.a3cb8832.chunk.js"
+  },
+  {
+    "revision": "5267f427a4746ffcc975",
+    "url": "/static/js/1.5267f427.chunk.js"
+  },
+  {
+    "revision": "a3cb8832df1f11d027f9",
+    "url": "/static/css/main.326c78fb.chunk.css"
+  },
+  {
+    "revision": "cfa892c5324d819c097a918cc674402f",
+    "url": "/index.html"
+  }
+];
