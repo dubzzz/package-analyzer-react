5c5077a39f4fc81d3152fa91d1d6608546371d8e
Nicolas DUBIEN <github@dubien.org>
2018-11-04 22:33:25 +0100

    Switch to HashRouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,14 @@ import store from './redux/store';
 
 import './index.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 import DetailsPage from './components/DetailsPage';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div id="main">
         <h1>Package Analyzer</h1>
         <Route exact path="/" component={SearchPage} />
