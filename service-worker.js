96f46cbb33b79ed4b1f02e03d259e060ae791e72
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-11 22:28:36 +0000

    Deploy dubzzz/package-analyzer-react to github.com/dubzzz/package-analyzer-react.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/package-analyzer-react/precache-manifest.e7c6760547ccbdffb03896231ea99f3b.js"
+  "/package-analyzer-react/precache-manifest.9ded3a4bcbe7939ebe8c706a304249bb.js"
 );
 
 workbox.clientsClaim();
